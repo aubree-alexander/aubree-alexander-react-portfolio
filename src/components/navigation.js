fb48841efa0061294c4eb3e3aa87baa116a550da
Aubree Alexander <aubree.alexander@gmail.com>
2022-07-16 11:04:54 -0500

    highlight currently selected nav items using navlink

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,32 +1,32 @@
 import React from 'react'
 import '../style.css'
-import {Link} from 'react-router-dom'
+import {NavLink} from 'react-router-dom'
 
-//AA - DO THE LINK PORTION FOR ALL NAV ITEMS
+//AA - DO THE NavLink PORTION FOR ALL NAV ITEMS
 function Navigation() {
     return (
         <div id='navigation'>
             <ul className='nav justify-content-center'>
-                <Link to = '/home'>
-                <li className='nav-item p-2'>
-                    About Me
-                </li>
-                </Link>
-                <Link to = '/contact'>
-                <li className='nav-item p-2'>
-                    Contact
-                </li>
-                </Link>
-                <Link to ='/portfolio'>
-                <li className='nav-item p-2'>
-                    Portfolio
-                </li>
-                </Link>
-                <Link to = '/resume'>
-                <li className='nav-item p-2'>
-                    Resume
-                </li>
-                </Link>
+                <NavLink to = '/home'>
+                    <li className='nav-item p-2'>
+                        About Me
+                    </li>
+                </NavLink>
+                <NavLink to = '/contact'>
+                    <li className='nav-item p-2'>
+                        Contact
+                    </li>
+                </NavLink>
+                <NavLink to ='/portfolio'>
+                    <li className='nav-item p-2'>
+                        Portfolio
+                    </li>
+                </NavLink>
+                <NavLink to = '/resume'>
+                    <li className='nav-item p-2'>
+                        Resume
+                    </li>
+                </NavLink>
             </ul>
         </div>
     )
