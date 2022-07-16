e0c135931d0db9e90034f9938c3d5de3dab77f36
Aubree Alexander <aubree.alexander@gmail.com>
2022-07-16 16:22:32 -0500

    adjust media queries

--- src/components/footer.js
+++ src/components/footer.js
@@ -14,23 +14,23 @@ import '../style.css'
 
 function Footer() {
     return (
-        <footer className=''>
-            <div className="contact-info py-2">
-                <ul>
+        <footer className='card mt-auto fixed-bottom d-flex text-center'>
+            <div className='contact-info py-2 card-footer d-flex justify-content-center'>
+                <ul className='navbar'>
                     <li>
-                        <i class="fa-solid fa-envelope p-2"></i>
+                        <i class="fa-solid fa-envelope p-2 nav-item"></i>
                         <a href="mailto:aubree.alexander@gmail.com">aubree.alexander@gmail.com</a>
                     </li>
                     <li>
-                        <i class="fa-solid fa-phone-flip p-2"></i>
+                        <i class="fa-solid fa-phone-flip p-2 nav-item"></i>
                         608.692.5277
                     </li>
                     <li>
-                        <i class="fa-brands fa-github p-2"></i>
+                        <i class="fa-brands fa-github p-2 nav-item"></i>
                         <a href="https://github.com/aubree-alexander" target="_blank">GitHub</a>
                     </li>
                     <li>
-                        <i class="fa-brands fa-linkedin p-2"></i>
+                        <i class="fa-brands fa-linkedin p-2 nav-item"></i>
                         <a href='https://www.linkedin.com/in/aubree-alexander-223a3518a' target="blank">LinkedIn</a>
                     </li>
                 </ul>
